b88e0848f3331a6d882a7a57a1a00a40ba88cfe3
Mohamed Abdelwahab <m.wahab_25@outlook.com>
2022-05-11 13:49:29 +0200

    Revert "commit"
    This reverts commit 4898765f704179a6dc9bd565f0f31c21552d697c.

--- KeywordDriven.Desktop/Form1.Designer.cs
+++ KeywordDriven.Desktop/Form1.Designer.cs
@@ -175,8 +175,12 @@ namespace KeywordDriven.Desktop
             this.groupBox5.Controls.Add(this.label11);
             this.groupBox5.Controls.Add(this.txt_projectname);
             this.groupBox5.Controls.Add(this.txt_generallocation);
-            this.groupBox5.Location = new System.Drawing.Point(11, 0);
+<<<<<<< HEAD
+            this.groupBox5.Location = new System.Drawing.Point(9, 0);
+=======
+            this.groupBox5.Location = new System.Drawing.Point(7, 7);
             this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(427, 88);
             this.groupBox5.TabIndex = 12;
@@ -248,8 +252,12 @@ namespace KeywordDriven.Desktop
             this.groupBox4.Controls.Add(this.label8);
             this.groupBox4.Controls.Add(this.label7);
             this.groupBox4.Controls.Add(this.txt_devicename);
-            this.groupBox4.Location = new System.Drawing.Point(11, 310);
+<<<<<<< HEAD
+            this.groupBox4.Location = new System.Drawing.Point(9, 248);
+=======
+            this.groupBox4.Location = new System.Drawing.Point(7, 317);
             this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(427, 160);
             this.groupBox4.TabIndex = 11;
@@ -343,8 +351,12 @@ namespace KeywordDriven.Desktop
             this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.combo_drivertype);
             this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Location = new System.Drawing.Point(11, 108);
+<<<<<<< HEAD
+            this.groupBox3.Location = new System.Drawing.Point(9, 86);
+=======
+            this.groupBox3.Location = new System.Drawing.Point(7, 114);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(427, 162);
             this.groupBox3.TabIndex = 10;
@@ -445,8 +457,12 @@ namespace KeywordDriven.Desktop
             // 
             this.btn_setup.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_setup.Enabled = false;
-            this.btn_setup.Location = new System.Drawing.Point(282, 518);
+<<<<<<< HEAD
+            this.btn_setup.Location = new System.Drawing.Point(226, 414);
+=======
+            this.btn_setup.Location = new System.Drawing.Point(354, 525);
             this.btn_setup.Margin = new System.Windows.Forms.Padding(4);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.btn_setup.Name = "btn_setup";
             this.btn_setup.Size = new System.Drawing.Size(141, 29);
             this.btn_setup.TabIndex = 22;
@@ -458,8 +474,12 @@ namespace KeywordDriven.Desktop
             // 
             this.btn_Execute.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Execute.Enabled = false;
-            this.btn_Execute.Location = new System.Drawing.Point(467, 518);
+<<<<<<< HEAD
+            this.btn_Execute.Location = new System.Drawing.Point(374, 414);
+=======
+            this.btn_Execute.Location = new System.Drawing.Point(539, 525);
             this.btn_Execute.Margin = new System.Windows.Forms.Padding(4);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.btn_Execute.Name = "btn_Execute";
             this.btn_Execute.Size = new System.Drawing.Size(157, 29);
             this.btn_Execute.TabIndex = 23;
@@ -500,7 +520,12 @@ namespace KeywordDriven.Desktop
             this.groupBox1.Controls.Add(this.label18);
             this.groupBox1.Controls.Add(this.label16);
             this.groupBox1.Controls.Add(this.label15);
-            this.groupBox1.Location = new System.Drawing.Point(555, 0);
+<<<<<<< HEAD
+            this.groupBox1.Location = new System.Drawing.Point(444, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+=======
+            this.groupBox1.Location = new System.Drawing.Point(548, 7);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(305, 408);
@@ -1282,13 +1307,19 @@ namespace KeywordDriven.Desktop
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(947, 563);
+<<<<<<< HEAD
+            this.ClientSize = new System.Drawing.Size(761, 457);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_Execute);
             this.Controls.Add(this.btn_setup);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
+=======
+            this.ClientSize = new System.Drawing.Size(1049, 645);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+>>>>>>> 00a89b0e32bd941fe06cd4a61c4d9244bac7be77
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
